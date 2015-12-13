5066671f1ac58de95b43d3e6803fed96dd7e63aa
hohohahi <hohohahi@qq.com>
2015-12-14 00:36:48 +0800

    1. debug

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var nodeUtil = require('./public/javascripts/nodeUtil.js');
+var nodeUtil = require('../public/javascripts/nodeUtil.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
