1b1cbcaadb25ead85377833a4ce2402788dc0af3
hohohahi <hohohahi@qq.com>
2015-12-14 00:22:09 +0800

    1. debug

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var app = express();
 
-
-var fs = require('fs');
-var nodeUtil = require('./public/javascripts/nodeUtil.js');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var nodeUtil = require('./public/javascripts/nodeUtil.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+app.post('/node', require('body-parser').json(), function(req, res){
+  var status = req.body.status;
+  var convertedStatus = nodeUtil.convertStatus(status);
 
-router.get('/node',  function(req, res){
-  res.send('test welcome.');
+  var isSupportedStatus = nodeUtil.isStatusSupported(convertedStatus);
+
+  var rtnCode = 200;
+  var isSuccess = true;
+  var rtnMessage = "Well done, mate.";
+
+  if (true == isSupportedStatus){
+    var newContent = nodeUtil.assembleNewContent(convertedStatus);
+    nodeUtil.writeContentToFile(newContent);
+
+    rtnMessage = newContent;
+    rtnCode = 200;
+    isSuccess = true;
+  }
+  else {
+    rtnMessage = "unsupported status. status:" + status;
+    rtnCode = 500;
+    isSuccess = false;
+  }
+
+  res.statusCode = rtnCode;
+  return res.json({success: isSuccess, message: rtnMessage});
 });
 
 module.exports = router;
