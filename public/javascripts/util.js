f35a3a6bdd3d7a12177ce1af5036353a345912b9
hohohahi <hohohahi@qq.com>
2015-12-14 21:31:51 +0800

    1. jquery ajax

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -1,14 +1,27 @@
+var stageUrlList = ["10.0.11.126", '10.0.11.126'];
 function ajaxGetServerStatus(){
-	$.ajax({
-		url: '/nodeStatus',
-		dataType: 'json',
-		cache: false,
-		timeout: 5000,
-		success: function(data){
-			alert(data.server);
-		},
-		error: function(jqXHR, textStatus, errorThrown){
-			alert('error ' + textStatus + " " + errorThrown);
-		}
-	});
+	var port = 8406;
+	var protocol = 'http';
+	var path = 'nodeStatus';
+
+	var jsonResult = {};
+	for (var i=0;i<stageUrlList.length;i++) {
+		var ipAddress = stageUrlList[i];
+		var fullUrl = protocol + '://' + ipAddress + ':' + port + '/' + path;
+
+		$.ajax({
+			url: fullUrl,
+			dataType: 'json',
+			cache: false,
+			timeout: 5000,
+			success: function(data){
+				jsonResult[ipAddress] = data.server;
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				alert('error ' + textStatus + " " + errorThrown);
+			}
+		});
+	}
+
+	JSON.stringify(jsonResult);
 }
